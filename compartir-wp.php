7c87ac318cb300384acce90fd279039647f2e51d
Eduardo Rengifo <eduardorengifo5@gmail.com>
2018-03-22 10:38:29 -0500

    Define constants for plugin dir

--- compartir-wp.php
+++ compartir-wp.php
@@ -9,3 +9,7 @@ License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  compartir-wp
 */
+
+define( 'COMPARTIR_WP__VERSION', '1.0.0' );
+define( 'COMPARTIR_WP__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'COMPARTIR_WP__PLUGIN_FILE', __FILE__ );
